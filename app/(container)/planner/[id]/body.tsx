3d6cc12e2b232a2ce1d66b17ea25cf9341b363f8
Darkness-Scholar <tlt.hp69@gmail.com>
2023-07-04 14:23:08 +0700

    test build 1

--- app/(container)/planner/[id]/body.tsx
+++ app/(container)/planner/[id]/body.tsx
@@ -9,6 +9,9 @@ const Body: React.FC<any> = () => {
         { id: "1", name: "tung", age: 24, job: "software engineer" },
         { id: "2", name: "hwang", age: 25, job: "content creator" },
         { id: "3", name: "darkness", age: 23, job: "joker" },
+        { id: "4", name: "darkness", age: 22, job: "joker" },
+        { id: "5", name: "darkness", age: 21, job: "joker" },
+        { id: "6", name: "darkness", age: 27, job: "joker" },
     ])
 
 
@@ -21,9 +24,8 @@ const Body: React.FC<any> = () => {
     };
 
     const getListStyle = isDraggingOver => ({
-        background: isDraggingOver ? "lightblue" : "lightgrey",
-        padding: grid,
-        width: 250
+        // background: isDraggingOver ? "lightblue" : "lightgrey",
+        padding: 0,
       });
 
     const handleOnDragEnd = (res: DropResult) => {
@@ -44,16 +46,15 @@ const Body: React.FC<any> = () => {
 
     const getItemStyle = (isDragging, draggableStyle) => ({
         userSelect: "none",
-        padding: grid * 2,
         margin: `0 0 ${grid}px 0`,
-
-        background: isDragging ? "lightgreen" : "grey",
+        backgroundImage: `url(https://picsum.photos/400/220)`,
+        // background: isDragging ? "lightgreen" : "grey",
 
         ...draggableStyle
     });
 
     return <div className="flex">
-        <div className="trip-date pl-8 py-4">
+        <div className="trip-date pl-8 py-4 w-[15%]">
             <p className='text-black text-lg font-semibold'>Start date</p>
             <button className="mr-4 px-4 py-1 border-[0.4px] bg-gray-200 rounded-md border-solid border-gray-300 text-gray-600">
                 Jul 08, 2023
@@ -74,17 +75,18 @@ const Body: React.FC<any> = () => {
             </div>
 
         </div>
-        <div className='px-10 py-4 bg-gray-100'>
-            <div className="flex items-center justify-center space-x-12">
+        <div className='w-[30%] px-10 py-4 bg-gray-100 max-h-[90vh] overflow-y-auto'>
+            <div className="mt-4 flex items-center justify-center space-x-12">
                 <p className="text-lg font-bold text-red-500">0 VND</p>
                 <p className="text-md font-bold text-orange-500">MAP VIEW</p>
             </div>
-            <p className="text-sm text-red-500">Clear day</p>
-            <div>
+            <p className="text-sm text-red-500 mb-2">Clear day</p>
+            <div className="mt-6">
                 <DragDropContext onDragEnd={handleOnDragEnd}>
                     <Droppable droppableId="droppable">
                         {(provided, snapshot) => (
                             <div
+                                className="grid grid-cols-1 gap-5"
                                 {...provided.droppableProps}
                                 ref={provided.innerRef}
                                 style={getListStyle(snapshot.isDraggingOver)}
@@ -96,6 +98,7 @@ const Body: React.FC<any> = () => {
                                                 ref={provided.innerRef}
                                                 {...provided.draggableProps}
                                                 {...provided.dragHandleProps}
+                                                className="px-6 py-8 drop-shadow-xl bg-center bg-cover rounded-lg"
                                                 style={getItemStyle(
                                                     snapshot.isDragging,
                                                     provided.draggableProps.style
@@ -112,6 +115,10 @@ const Body: React.FC<any> = () => {
                     </Droppable>
                 </DragDropContext>
             </div>
+        </div>
+
+        <div className="w-[55%] bg-white">
+            
         </div>
     </div>
 }
